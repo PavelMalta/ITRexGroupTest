085970c56dccb4e5b6dd0ce8c3355813e41ad246
PavelMalta <shamashov87@mail.ru>
2021-07-09 15:48:04 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import s from './App.module.css';
+import React from "react";
+import {v1} from "uuid";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const data = [
+        {
+            name: "Dollars",
+            unit: 1,
+            rate: 2.5,
+        },
+        {
+            name: "Euros",
+            unit: 1,
+            rate: 3
+        },
+        {
+            name: "RUB",
+            unit: 100,
+            rate: 0.7
+        },
+    ]
+
+    return (
+        <div className={s.app}>
+            <div className={s.wrap}>
+            <table className={s.table}>
+                <tr className={s.header}>
+                    <th className={s.column}>Foreign currency denomination</th>
+                    <th className={s.column}>Number of units of foreign currency, letter code of the currency</th>
+                    <th className={s.column}>Official rate</th>
+                </tr>
+                {data.map((item) => {
+                    return (
+                        <StringTable key={v1()}
+                                     name={item.name}
+                                     unit={item.unit}
+                                     rate={item.rate}
+                        />
+                    )
+                })}
+            </table>
+        </div>
+        </div>
+    );
 }
+
+export const StringTable = (props) => {
+
+    return (
+        <tr className={s.row}>
+            <td>{props.name}</td>
+            <td>{props.unit}</td>
+            <td>{props.rate}</td>
+        </tr>
+    )
+}
+
 
 export default App;
