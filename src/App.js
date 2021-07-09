c1a32ade0f62ab271fb93dee0a2a97d23ed7579a
PavelMalta <shamashov87@mail.ru>
2021-07-09 17:08:55 +0300

    added Table component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import s from './App.module.css';
 import React, {useEffect, useState} from "react";
-import {v1} from "uuid";
 import {currencyAPI} from "./currency-api";
+import {Table} from "./table/Table";
 
 function App() {
 
@@ -16,38 +16,9 @@ function App() {
 
     return (
         <div className={s.app}>
-            <div className={s.wrap}>
-                <table className={s.table}>
-                    <tr className={s.header}>
-                        <th className={s.column}>Foreign currency denomination</th>
-                        <th className={s.column}>Number of units of foreign currency, letter code of the currency</th>
-                        <th className={s.column}>Official rate</th>
-                    </tr>
-                    {data.map((item) => {
-                        return (
-                            <StringTable key={v1()}
-                                         name={item.Cur_Name}
-                                         unit={item.Cur_Scale}
-                                         rate={item.Cur_OfficialRate}
-                                         letterCode={item.Cur_Abbreviation}
-                            />
-                        )
-                    })}
-                </table>
-            </div>
+           <Table data={data}/>
         </div>
     );
-}
-
-export const StringTable = (props) => {
-
-    return (
-        <tr className={s.row}>
-            <td>{props.name}</td>
-            <td>{props.unit} {props.letterCode}</td>
-            <td>{props.rate}</td>
-        </tr>
-    )
 }
 
 
---
+++ src/table/Table.js
@@ -0,0 +1,28 @@
+import React from "react";
+import s from "../App.module.css";
+import {StringTable} from "./StringTable";
+import {v1} from "uuid";
+
+export const Table = (props) => {
+    return (
+        <div className={s.wrap}>
+            <table className={s.table}>
+                <tr className={s.header}>
+                    <th className={s.column}>Наименование иностранной валюты</th>
+                    <th className={s.column}>Количество единиц иностранной валюты, буквенный код валюты</th>
+                    <th className={s.column}>Официальный курс</th>
+                </tr>
+                {props.data.map((item) => {
+                    return (
+                        <StringTable key={v1()}
+                                     name={item.Cur_Name}
+                                     unit={item.Cur_Scale}
+                                     rate={item.Cur_OfficialRate}
+                                     letterCode={item.Cur_Abbreviation}
+                        />
+                    )
+                })}
+            </table>
+        </div>
+    )
+}
